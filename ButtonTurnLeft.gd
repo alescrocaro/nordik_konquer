d7f54507cb8cb813fa2b3505986600cd835480f8
alescrocaro <alescrocaro@gmail.com>
2023-10-11 00:39:52 -0300

    Add cards to turn left and right

---
+++ ButtonTurnLeft.gd
@@ -0,0 +1,11 @@
+extends Button
+
+@onready var player = get_node('/root/GameManager/Player')
+@onready var playerSprite: Sprite2D = get_node('/root/GameManager/Player/playerSprite')
+
+
+func _pressed() -> void:
+	var currentFrame = playerSprite.get_frame()
+	playerSprite.set_frame(currentFrame + 1 if currentFrame < 3 else 0)
+	
+	
